082f03894b3a54e5b10d7b815a23dcdeabf2dae7
Urho Tamminen <urho.tamminen@sito.fi>
2014-07-22 07:07:31 +0000

    add controller for indicators

---
+++ LiiteriDataAPI/Controllers/IndicatorController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+
+using LiiteriStatisticsCore.Repositories;
+using LiiteriStatisticsCore.Models;
+using LiiteriStatisticsCore.Queries;
+
+namespace LiiteriDataAPI.Controllers
+{
+    public class IndicatorController : ApiController
+    {
+        private DbConnection GetDbConnection(bool open = true)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings[
+                "LiiteriDB"].ToString();
+            DbConnection db = new SqlConnection(connStr);
+            if (open) db.Open();
+            return db;
+        }
+
+        [Route("v1/indicators/")]
+        [HttpGet]
+        public IEnumerable<IndicatorBrief> GetIndicators()
+        {
+            var query = new IndicatorQuery();
+            using (DbConnection db = this.GetDbConnection()) {
+                var repository = new IndicatorBriefRepository(db);
+                return (List<IndicatorBrief>) repository.FindAll(query);
+            }
+        }
+
+        [Route("v1/indicators/{id}")]
+        [HttpGet]
+        public IndicatorDetails GetIndicatorDetails(int id)
+        {
+            var query = new IndicatorQuery();
+            query.IdIs = id;
+
+            using (DbConnection db = this.GetDbConnection()) {
+                var repository = new IndicatorDetailsRepository(db);
+                return (IndicatorDetails) repository.Single(query);
+            }
+        }
+    }
+}
