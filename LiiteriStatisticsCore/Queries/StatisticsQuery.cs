0124c00672a5c1d6885026abf1c09ef04faee169
Urho Tamminen <urho.tamminen@sito.fi>
2014-08-01 12:55:31 +0000

    add initial code for summed/derived calculations (non-functional in this revision)

--- LiiteriStatisticsCore/Queries/StatisticsQuery.cs
+++ LiiteriStatisticsCore/Queries/StatisticsQuery.cs
@@ -54,33 +54,6 @@ namespace LiiteriStatisticsCore.Queries
             set
             {
                 this.AddParameter("@YearIs", value);
-                this.whereList.Add(string.Format("T.Jakso_ID = {0}", value));
-            }
-        }
-
-        private int[] _YearIn;
-        public int[] YearIn
-        {
-            get
-            {
-                return this._YearIn;
-            }
-            set
-            {
-                if (value == null) return;
-                if (this._YearIn != null) {
-                    throw new ArgumentException("Value already set!");
-                }
-                this._YearIn = value;
-                string[] paramNames = value.Select(
-                        (s, i) => "@YearIn_" + i.ToString()
-                    ).ToArray();
-                for (int i = 0; i < paramNames.Length; i++) {
-                    this.AddParameter(paramNames[i], value[i]);
-                }
-                this.whereList.Add(string.Format(
-                    "T.Jakso_ID IN ({0})",
-                    string.Join(",", paramNames)));
             }
         }
 
@@ -105,7 +78,89 @@ namespace LiiteriStatisticsCore.Queries
         /* Grouping */
         public int GroupByAreaTypeIdIs { get; set; }
 
-        public override string GetQueryString()
+        private string GetQueryString_SummedDivided()
+        {
+            IDictionary<string, string> fields =
+                new Dictionary<string, string>();
+            List<string> groups = new List<string>();
+            StringBuilder sbFrom = new StringBuilder();
+
+            fields["T1.Jakso_ID"] = "Year";
+            groups.Add("T1.Jakso_ID");
+
+            if (this.FilterAreaIdIs != null &&
+                    this.FilterAreaTypeIdIs != null) {
+                string column = AreaTypeMappings.GetAreaColumn(
+                    (int) this.FilterAreaTypeIdIs);
+                this.whereList.Add(string.Format(
+                    "A.{0} = @FilterAreaIdIs", column));
+            }
+
+            fields["(SUM(T1.Arvo) / SUM(T2.Arvo))"] = "Value";
+
+            fields["A2.Alue_ID"] = "AreaId";
+            groups.Add("A2.Alue_ID");
+
+            fields["A2.Nimi"] = "AreaName";
+            groups.Add("A2.Nimi");
+
+            /* add the proper table that we are grouping by */
+            sbFrom.Append(string.Format(
+                "INNER JOIN {0} A2 ON A2.Alue_ID = A.{1} ",
+                AreaTypeMappings.GetAreaTable(this.GroupByAreaTypeIdIs),
+                AreaTypeMappings.GetAreaColumn(this.GroupByAreaTypeIdIs)));
+
+            string queryString = @"
+SELECT
+    {0}
+FROM
+    DimTilasto_JohdettuTilasto_Jako J
+
+    LEFT JOIN FactTilastoarvo T1 ON
+        T1.Tilasto_ID = J.Osoittaja_Tilasto_ID AND
+        T1.Jakso_ID = @YearIs AND
+        T1.AlueTaso_ID = @DatabaseAreaTypeIdIs
+
+    LEFT JOIN FactTilastoarvo T2 ON
+        T2.Tilasto_ID = J.Nimittaja_Tilasto_ID AND
+        T2.Jakso_ID = @YearIs AND
+        T2.AlueTaso_ID = @DatabaseAreaTypeIdIs
+
+    INNER JOIN DimAlue A ON
+        A.Alue_ID = T1.Alue_ID AND
+        A.Alue_ID = T2.Alue_ID AND
+        @YearIs BETWEEN A.Alkaen_Jakso_ID AND A.Asti_Jakso_ID AND
+        A.AlueTaso_ID = @DatabaseAreaTypeIdIs
+    {1}
+WHERE
+    J.Tilasto_ID = @IdIs
+    {2}
+GROUP BY
+    {3}
+";
+            string fieldsString = string.Join<string>(", ", (
+                    from pair in fields
+                    select string.Format("{0} AS {1}", pair.Key, pair.Value)
+                ).ToArray());
+
+            string groupString =
+                string.Join<string>(", ", groups);
+
+            string whereString = "";
+            if (this.whereList.Count > 0) {
+                whereString = " AND " + string.Join(" AND ", this.whereList);
+            }
+
+            queryString = string.Format(queryString,
+                fieldsString,
+                sbFrom.ToString(),
+                whereString,
+                groupString);
+
+            return queryString;
+        }
+
+        private string GetQueryString_Normal()
         {
             IDictionary<string, string> fields =
                 new Dictionary<string, string>();
@@ -124,26 +179,20 @@ namespace LiiteriStatisticsCore.Queries
                     "A.{0} = @FilterAreaIdIs", column));
             }
 
-            switch (this.CalculationTypeIdIs) {
-                case 1: // normal
+            fields["SUM(T.Arvo)"] = "Value";
 
-                    fields["SUM(T.Arvo)"] = "Value";
+            fields["A2.Alue_ID"] = "AreaId";
+            groups.Add("A2.Alue_ID");
 
-                    fields["A2.Alue_ID"] = "AreaId";
-                    groups.Add("A2.Alue_ID");
+            fields["A2.Nimi"] = "AreaName";
+            groups.Add("A2.Nimi");
 
-                    fields["A2.Nimi"] = "AreaName";
-                    groups.Add("A2.Nimi");
+            /* add the proper table that we are grouping by */
+            sbFrom.Append(string.Format(
+                "INNER JOIN {0} A2 ON A2.Alue_ID = A.{1} ",
+                AreaTypeMappings.GetAreaTable(this.GroupByAreaTypeIdIs),
+                AreaTypeMappings.GetAreaColumn(this.GroupByAreaTypeIdIs)));
 
-                    /* add the proper table that we are grouping by */
-                    sbFrom.Append(string.Format(
-                        "INNER JOIN {0} A2 ON A2.Alue_ID = A.{1} ",
-                        AreaTypeMappings.GetAreaTable(this.GroupByAreaTypeIdIs),
-                        AreaTypeMappings.GetAreaColumn(this.GroupByAreaTypeIdIs)));
-
-                    /* then add filtering here */
-                    break;
-            }
             
             string queryString = @"
 SELECT
@@ -158,7 +207,8 @@ FROM
 WHERE
 	T.Tilasto_ID = @IdIs AND
 	T.AlueTaso_ID = @DatabaseAreaTypeIdIs AND
-	T.Arvo IS NOT NULL
+	T.Arvo IS NOT NULL AND 
+    T.Jakso_ID = @YearIs
     {2}
 GROUP BY
     {3}
@@ -183,10 +233,29 @@ GROUP BY
                 whereString,
                 groupString);
 
+            return queryString;
+        }
+
+        public override string GetQueryString()
+        {
+            string queryString;
+
+            switch (this.CalculationTypeIdIs) {
+                case 1: // normal
+                    queryString = this.GetQueryString_Normal();
+                    break;
+                case 3: // summed & divided
+                    queryString = this.GetQueryString_SummedDivided();
+                    break;
+                default:
+                    throw new Exception(string.Format(
+                        "Unsupported CalculationType: {0}",
+                        this.CalculationTypeIdIs));
+            }
+
             foreach (var param in this.Parameters) {
                 Debug.WriteLine("DECLARE {0} INT = {1}", param.Key, param.Value);
             }
-
             Debug.WriteLine(queryString);
 
             return queryString;
