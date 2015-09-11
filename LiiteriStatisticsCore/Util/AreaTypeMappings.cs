a4be2b12c0dca3ac6288bbfc3dfe0c3adef69831
Urho Tamminen <urho.tamminen@sito.fi>
2015-09-11 08:01:35 +0000

    merge from branch:
    r3687:
    
    Fix GetPrimaryAreaType incorrectly returning the first primary areatype of any databaseAreaType
    
    This caused the special statistics (type 4) indicators to return incorrect areatypes. Bug originally introduced in revision 1856.

--- LiiteriStatisticsCore/Util/AreaTypeMappings.cs
+++ LiiteriStatisticsCore/Util/AreaTypeMappings.cs
@@ -205,6 +205,7 @@ namespace LiiteriStatisticsCore.Util
                         .Descendants("DatabaseAreaTypes")
                         .Descendants("DatabaseAreaType")
                     where
+                        Convert.ToInt32(d.Attribute("id").Value) == databaseAreaTypeId &&
                         d.Attribute("primary") != null &&
                         d.Attribute("primary").Value.ToLower() == "true"
                     select d);
